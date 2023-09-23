44fba0302136c8314aaf8c65dd747ea3bd28cda4
Jacob Wyngaard <wyngaardjt@hotmail.com>
2023-09-23 13:06:07 -0400

    Updates

--- +Gambling/system_utility.m
+++ +Gambling/system_utility.m
@@ -1,7 +1,5 @@
 function value = system_utility(~,gamma,pbeta,palpha)
 % Takes density, gamma, vLow, vHigh
 
-% Returns the value of the equation corresponding to the balance constraint
-% of a classical money line
     value = (1-pbeta./palpha).*(gamma(1)-gamma(palpha));
 end
