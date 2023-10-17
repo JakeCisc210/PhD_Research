23eb8a91482d2ea42dac963cc64065981a81e84c
Wyngaard, Jacob, T <jacob.wyngaard@jhuapl.edu>
2023-10-17 13:23:25 -0400

    First Commit during Lunch Break at Work

--- Startup_Script.m
+++ Startup_Script.m
@@ -1,3 +1,4 @@
 % User Start Up
 
-addpath(genpath('C:\Users\wynga\OneDrive\Desktop\PhD_Research'))
+addpath(genpath('C:\Users\wyngajt1\Desktop\PhD_Research'))
+% addpath(genpath('C:\Users\wynga\OneDrive\Desktop\PhD_Research'))
